0c699afc049c54ffa783e39c837a5159155b326c
myname21-meet <myname21@meet.mit.edu>
2019-07-29 03:28:36 -0400

    cool name

--- knowledge_model.py
+++ knowledge_model.py
@@ -6,12 +6,46 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
-	# Create a table with 4 columns
-	# The first column will be the primary key
-	# The second column should be a string representing
-	# the name of the Wiki article that you're referencing
-	# The third column will be a string representing the 
-	# topic of the article. The last column will be
-	# an integer, representing your rating of the article.
+	__tablename__='articles'
+	articles_id=Column(Integer, primary_key = True)
+	name = Column(String )
+   	topic= Column(String)
+   	rate = Column(Integer)
 
-	pass
+
+   	def __repr__(self):
+		return ("Article Name: {}\n"
+               	"Topic: {} \n"
+                "Rate: {}").format(
+                    self.name,
+                    self.topic, 
+                    self.rate)
+                    
+
+
+article1= Knowledge(name="Donald Trump", topic="Prime Ministers" , rate= 6 )
+print(article1)
+
+article2= Knowledge(name="Little Mix", topic="Bands" , rate= 10 )
+print(article2)
+
+article3= Knowledge(name="The Notebook", topic="Movies" , rate= 8 )
+print(article3)
+
+article4=Knowledge(name= "rainbow", topic="weather", rate= 9 )
+print(article4)
+
+
+    
+    
+
+
+
+
+
+
+	
+
+
+
+
